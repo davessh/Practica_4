9d397d29a6bf7d27326925412c03202fa2ca470c
David <david.schlemmer@uabc.edu.mx>
2025-03-31 12:43:45 -0700

    corrección1.0@

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     //hola mundo
 
     public static void main(String[] args) {
-        System.out.println("Hello marriana");
+        System.out.println("Hello marriana1");
     }
 }
